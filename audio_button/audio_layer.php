318948473fa9e1821c489c0be9ca290ffc390987
Scott Saunders <scott.saunders@Saunders-S.local>
2021-10-07 14:21:15 -0400

    converted material icons to svgs.

--- audio_button/audio_layer.php
+++ audio_button/audio_layer.php
@@ -2,8 +2,8 @@
         <div class="audio-box">
             
             <div class="audio-controls">
-                <span class="hide-player"><span class="material-icons">remove</span></span>
-                <span class="close-player"><span class="material-icons">close</span></span>
+                <span class="hide-player"><svg><path d="M13.2,8.7H2.8V7.3h10.5V8.7z"/></svg></span>
+                <span class="close-player"><svg><path d="M13,4l-1-1L8,7L4,3L3,4l4,4l-4,4l1,1l4-4l4,4l1-1L9,8L13,4z"/></svg></span>
             </div>
             
             <audio id="audio" preload="auto" tabindex="0" controls>
