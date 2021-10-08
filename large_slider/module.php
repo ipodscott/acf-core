0ad8a24500cd7ea96349c2f5f32f777b28fcd0ec
Scott Saunders <scott.saunders@Saunders-S.local>
2021-10-08 00:00:36 -0400

    added large slider

---
+++ large_slider/module.php
@@ -0,0 +1,28 @@
+<?php
+	// Create Block Attributes
+	acf_register_block(array(
+	'name'				=> 'featured_swiper_block',
+	'title'				=> __('Featured Swiper Block'),
+	'description'		=> __('Featured Swiper Block'),
+	'mode'		        => 'preview',
+	'render_template' => plugin_dir_path( __FILE__ ) . 'template.php',
+	'category'			=> 'custom-blocks',
+	'enqueue_style'     =>  plugin_dir_url( __FILE__ ). 'css/swiper.css',
+	'enqueue_script'    =>  plugin_dir_url( __FILE__ ). 'js/swiper-bundle.min.js', array('jquery'), '1.0', true,
+	'icon'				=> '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 6h16v2H4zm2-4h12v2H6zm14 8H4c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2zm0 10H4v-8h16v8zm-10-7.27v6.53L16 16z"/></svg>',
+	'keywords'			=> array( 'slider' ),
+));
+
+
+add_action('wp_footer', 'add_large_slider_layer');
+function add_large_slider_layer(){
+	wp_enqueue_script( 'lg_slider_js', plugin_dir_url( __FILE__ ). 'js/lg_slider.js', array('jquery'), '1.0', true );
+};
+
+
+// Read local acf.json
+$acf_json_data = ( plugin_dir_path( __FILE__ ) . 'acf.json' );
+$custom_fields = $acf_json_data ? json_decode( file_get_contents( $acf_json_data ), true ) : array();
+foreach ( $custom_fields as $custom_field ) {
+   acf_add_local_field_group( $custom_field );
+}
---
+++ large_slider/template.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Block Name: Swiper Box
+ *
+ * This is the template that displays a swiper.
+ */
+// create id attribute for specific styling
+$id = $block['id'];
+?>
+
+	<div data-aos="fade-in" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" class="big-slider featured-swiper-container">
+	    <div class="swiper-wrapper">
+		    
+		    
+		    
+<?php while(has_sub_field("slide_selector")): ?>
+	<?php if(get_row_layout() == "library_slide"):?>
+	
+	
+				<?php 
+				    $posts = get_sub_field('video_slide');
+					if( $posts ): ?>
+					
+					<?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+					    <div class="swiper-slide">
+							<div class="slide-inside bg-1" style="background-image: url(<?php the_field('slider_image', $p->ID);?>);">
+								<div class="slide-overlay">
+									<a class="slide-details fadey-fast move-down" href="<?php the_permalink($p->ID);?>">
+										<div class="slide-title"><div class="left-title"></div><div class="borders"><?php the_field('slide_title', $p->ID);?></div><div class="right-title"></div></div>
+										<div class="slide-copy"><?php the_field('display_copy', $p->ID);?></div>
+										<div class="slide-button">= <?php the_field('button_title', $p->ID);?> =</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+												
+				<?php endif; ?>
+	 		 
+			<?php elseif(get_row_layout() == "custom_slide"):?>
+	 			
+	 				<div class="swiper-slide">
+						<div class="slide-inside bg-1" style="background-image: url(<?php the_sub_field('image');?>);">
+							<div class="slide-overlay">
+								<a <?php if( get_sub_field('open_in_new_tab') ): ?> target="_blank" <?php endif; ?> class="slide-details fadey-fast move-down" href="<?php the_sub_field('link_url');?>">
+									<div class="slide-title"><div class="left-title"></div><div class="borders"><?php the_sub_field('title');?></div><div class="right-title"></div></div>
+									<div class="slide-copy"><?php the_sub_field('copy');?></div>
+									<div class="slide-button">= <?php the_sub_field('link_title');?> =</div>
+								</a>
+							</div>
+						</div>
+					</div>
+			 
+				
+	<?php endif; ?>
+<?php endwhile; ?>
+		    
+		    
+		    
+		    		
+		    
+			
+			
+    </div>
+    <!-- Add Pagination -->
+    <div class="progress-swiper-pagination"></div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
