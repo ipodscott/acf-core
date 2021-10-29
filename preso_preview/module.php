be882ff5c72a3cec4c754238f0af6b688fdd4188
Scott <scottsaunders@mac.com>
2021-10-28 22:36:02 -0400

    added assets to preo preview

---
+++ preso_preview/module.php
@@ -0,0 +1,89 @@
+<?php
+	// Created Block Attributes
+	acf_register_block(array(
+	'name'				=> 'presentation_preview',
+	'title'				=> __('Presentation Preview'),
+	'description'		=> __('Block for previewing presentations. Requires slides based presentations directory.'),
+	'render_template'   => plugin_dir_path( __FILE__ ) . 'template.php',
+	'post_types'        => array('page','post'),
+	'mode'              => 'preview',
+	'category'			=> 'acf-core-blocks',
+	'icon'				=> '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"> 
+	<path d="M21.4,1.6H2.6c-1.2,0-2.1,0.9-2.1,2.1v12.5c0,1.1,0.9,2.1,2.1,2.1h7.3v2.1H7.8v2.1h8.3v-2.1h-2v-2.1h1.8h5.5
+		c1.2,0,2.1-0.9,2.1-2.1V3.7C23.5,2.5,22.6,1.6,21.4,1.6z M21.4,16.2h-5.2H2.6V3.7h18.8V16.2z"/>
+	<polygon points="9.4,13.7 12,11.9 14.6,13.7 13.6,10.7 16.2,8.9 13,8.9 12,5.7 11,8.9 7.8,8.9 10.4,10.7 "/></svg> ',
+	'keywords'			=> array( 'columns' ),
+	'enqueue_style'     =>  plugin_dir_url( __FILE__ ). 'css/styles.css',
+    'enqueue_script'    =>  plugin_dir_url( __FILE__ ). 'js/scripts.js', array('jquery'), '1.0', true,
+	
+	'supports'		=> [
+		'align'			=> false,
+		'anchor'		=> true,
+		'customClassName'	=> true,
+		'customIdName'	=> true,
+		'mode'          => true,
+		]
+));
+
+
+// Created Block Attributes
+	acf_register_block(array(
+	'name'				=> 'presentation_gallery',
+	'title'				=> __('Presentation Gallery'),
+	'description'		=> __('Block for creating a presentation gallery.'),
+	'render_template'   => plugin_dir_path( __FILE__ ) . 'presentation_template.php',
+	'post_types'        => array('presentation'),
+	'mode'              => 'preview',
+	'category'			=> 'acf-core-blocks',
+	'multiple'          =>  false,
+	'icon'				=> '<svg viewBox="0 0 24 24"><path d="M21.4,1.6H2.6c-1.2,0-2.1,0.9-2.1,2.1v12.5c0,1.1,0.9,2.1,2.1,2.1h7.3v2.1H7.8v2.1h8.3v-2.1h-2v-2.1h1.8h5.5
+		c1.2,0,2.1-0.9,2.1-2.1V3.7C23.5,2.5,22.6,1.6,21.4,1.6z M21.4,16.2h-5.2H2.6V3.7h18.8V16.2z"/><polygon points="9.4,13.7 12,11.9 14.6,13.7 13.6,10.7 16.2,8.9 13,8.9 12,5.7 11,8.9 7.8,8.9 10.4,10.7 "/></svg>',
+	'keywords'			=> array( 'columns' ),
+	'enqueue_style'     =>  plugin_dir_url( __FILE__ ). 'css/preso_styles.css',
+    'enqueue_script'    =>  plugin_dir_url( __FILE__ ). 'js/preso_scripts.js', array('jquery'), '1.0', true,
+	
+	'supports'		=> [
+		'align'			=> false,
+		'anchor'		=> true,
+		'customClassName'	=> true,
+		'customIdName'	=> true,
+		'mode'          => true,
+		]
+));
+
+
+
+add_action('wp_footer', 'add_preso_layer');
+function add_preso_layer(){
+	require_once( plugin_dir_path( __FILE__ ) . '/preso_loader.php');
+};
+
+// Read local acf.json
+$acf_json_data = ( plugin_dir_path( __FILE__ ) . 'acf.json' );
+$custom_fields = $acf_json_data ? json_decode( file_get_contents( $acf_json_data ), true ) : array();
+foreach ( $custom_fields as $custom_field ) {
+   acf_add_local_field_group( $custom_field );
+}
+
+
+
+
+// Our custom post type function for presentations
+function create_posttype_presentations() {
+
+	register_post_type( 'presentation',
+	// CPT Options
+		array(
+			'labels' => array(
+				'name' => __( 'Presentations' ),
+				'singular_name' => __( 'Presentation' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'presentation'),
+			'show_in_rest' => true,
+      		)
+	);
+}
+add_action( 'init', 'create_posttype_presentations' );
---
+++ preso_preview/preso_loader.php
@@ -0,0 +1,12 @@
+<div class="preso-loader" style="display:none;">
+	
+	<div class="preso-box">
+		<iframe frameborder="0" class="presoFrame" src=""></iframe>
+	</div>
+
+	<div class="close-preso">
+		<svg><path style="fill:#fff;" d="M12,2C6.5,2,2,6.5,2,12s4.5,10,10,10s10-4.5,10-10S17.5,2,12,2z M17,15.6L15.6,17L12,13.4L8.4,17L7,15.6
+		l3.6-3.6L7,8.4L8.4,7l3.6,3.6L15.6,7L17,8.4L13.4,12L17,15.6z"/></svg>
+	</div>
+	
+</div>
