9d8a39d97ba7c38d4c545b737759761289efe0ab
Scott <scottsaunders@mac.com>
2022-03-21 21:22:57 -0400

    readded presentation slide

---
+++ presentation-slide/module.php
@@ -0,0 +1,28 @@
+<?php
+// Presentation Block
+acf_register_block( array(
+   'name'            => 'presentation-slide',
+   'title'           => __( 'Presentation Slide' ),
+   'description'     => __( 'Single Slide block for a presentation.' ),
+   'render_template' => plugin_dir_path( __FILE__ ) . 'template.php',
+   'mode'            => 'preview',
+   'category'        => 'custom-blocks',
+   'multiple'        => true,
+   'icon'            => 'slides',
+   'keywords'        => array( 'accordion' ),
+   'enqueue_style'     =>  plugin_dir_url( __FILE__ ). 'css/style.css',
+   'enqueue_script'    =>  plugin_dir_url( __FILE__ ). 'js/script.js', array('jquery'), '1.0', true,
+   array( 'jquery' ),
+   '1.0',
+   true,
+   'supports'        => [
+	  'mode'           => false,
+      'align'           => false,
+      'anchor'          => true,
+      'customClassName' => true,
+      'jsx'             => true,
+   ]
+) );
+// Read local acf.json
+$acf_json_data = ( plugin_dir_path( __FILE__ ) . 'acf.json' );
+$custom_fields = $acf_json_data ? json_decode( file_get_contents( $acf_json_data ), true ) : array();
---
+++ presentation-slide/template.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Modal Video Block
+ *
+ * This is the template for a modal video button.
+ */
+ 
+ 	if( ! function_exists('presentation_preview') ) {
+	
+		// Add Video CSS and JS 
+		wp_enqueue_style( 'presentation_slide_style', plugin_dir_url( __FILE__ ) . 'css/style.css',true,'1.1','all' );
+		
+		function presentation_preview() 
+		{ // Adds video styles to preview content in the backend.
+		    wp_enqueue_style( 'presentation_slide_preview', plugin_dir_url( __FILE__ ) . 'css/style.css',true,'1.1','all' );
+		}
+		add_action('admin_footer', 'presentation_preview');	
+	}
+	
+
+	// Create id attribute allowing for custom "anchor" value.
+	$id = $block['id'];
+	if( !empty($block['anchor']) ) {
+	    $id = $block['anchor'];
+	}
+	
+	// Create class attribute allowing for custom "className" and "align" values.
+	$className = '';
+	if( !empty($block['className']) ) {
+	    $className .= ' ' . $block['className'];
+	}
+	
+	$slide_name = get_field( 'slide_name' );
+	$slide_slug = '' . sanitize_title( $slide_name ) . '';
+	$background_image = get_field( 'background_image' );
+	$use_sub_slides = get_field( 'use_sub_slides' );
+	$make_sub_slide = get_field( 'make_sub_slide' );
+	$background_type = get_field( 'background_type' );
+	$image = get_field( 'image' );
+	$color = get_field( 'color' );
+	
+	if( $background_video == 'upload' ) : 
+		$vid_bg = $bg_video_upload;
+	 
+	 elseif( $background_video == 'remote' ): 
+	 	$vid_bg = $bg_video_url;
+	 	
+	 endif 
+	
+	?>
+	<?php if( !$make_sub_slide ) : ?>
+	  
+			<?php if( !$use_sub_slides ) : ?>
+				<?php if( is_admin() ):?><div class="slide-handle"><span>Slide</span></div><?php endif ?>
+				<li class="preso-slide" id="<?php echo $slide_slug; ?>">
+					<div class="cd-slider-content">
+						<div class="content-wrapper">
+							<div class="preso-content"><?php echo '<InnerBlocks />';?></div>
+						</div>
+					</div>
+				</li>
+		
+				  
+			<?php  else: ?> 
+				<?php if( is_admin() ):?><div class="slide-handle"><span>Sub Slide Container</span></div><?php endif ?>
+				<li class="preso-slide sub-slide-container" id="<?php echo $slide_slug; ?>">
+					<ol class="sub-slides">
+						<?php echo '<InnerBlocks />';?>
+					</ol>
+				</li>
+			  
+			<?php endif ?>
+		
+		<?php  else: ?> 
+				<?php if( is_admin() ):?><div class="slide-handle"><span>Sub Slide</span></div><?php endif ?>
+				<li class="preso-slide" id="<?php echo $slide_slug; ?>">
+					<div class="cd-slider-content">
+						<div class="content-wrapper">
+							<div class="preso-content"><?php echo '<InnerBlocks />';?></div>		
+						</div>
+					</div>
+				</li>
+		
+	<?php endif ?>
+	  
