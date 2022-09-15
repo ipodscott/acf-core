a7d6dd8579ca1c5b3709ddc50a40ad48633a0e66
scott <scottsaunders@mac.com>
2022-09-15 13:38:45 -0400

    adding missing includes
    Added missing includes after update.

--- acf-core.php
+++ acf-core.php
@@ -23,6 +23,24 @@ function my_acf_init()
   // check function exists
   if (function_exists("acf_register_block")) {
     include_once "api_block/module.php";
+    include_once "audio_button/module.php";
+    include_once "big_book/module.php";
+    include_once "primary_content_block/module.php";
+    include_once "gallery_block/module.php";
+    include_once "large_header/module.php";
+    include_once "link_button/module.php";
+    include_once "parallax_header/module.php";
+    include_once "presentation-slide/module.php";
+    //include_once "media-button/module.php";
+    include_once "video_button/module.php";
+    //include_once 'preso_preview/module.php';
+    include_once "large_slider/module.php";
+    include_once "video_summary/module.php";
+    include_once "slide/module.php";
+    include_once "side_navigation/module.php";
+    //include_once 'image_zoom/module.php';
+    include_once "big_menu/module.php";
+    //include_once 'primary_flex_container/module.php';
   }
 }
 
