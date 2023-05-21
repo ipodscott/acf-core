c77dd7bf4e53fce690d757b5298009bda531b726
scott <scottsaunders@mac.com>
2023-05-21 00:46:31 -0400

    Update video-btn.js
    Converted back to Jquery

--- video_button/js/video-btn.js
+++ video_button/js/video-btn.js
@@ -1,92 +1,151 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-
-	function videoChange(){
-		document.querySelector('.default-video-cover').style.display = 'none';
-		document.querySelector('.vid-box').style.display = 'none';
-
-		setTimeout(function(){
-			document.querySelector('.vid-box').style.display = 'block';
-		}, 500);
-
-		setTimeout(function(){
-			document.querySelector('.main-menu').style.display = 'none';
-			document.querySelector('.menu-overlay').style.display = 'none';
-		}, 500);
-	}
-
-	let videoMenuItems = document.querySelectorAll(".video-menu li");
-	videoMenuItems.forEach(function (item) {
-		item.addEventListener('click', function () {
-			videoMenuItems.forEach(function (i) {
-				i.classList.remove("show");
+(function($){
+	$( document ).ready(function() {
+		
+		function videoChange(){
+			$('.default-video-cover').fadeOut(500);
+			
+			$('.vid-box').fadeOut(500, function(){
+				$('.vid-box').delay(500).fadeIn(500);
 			});
-			this.classList.add("show");
+		
+			$('.main-menu').delay(500).fadeOut(500, function(){
+				$('.menu-overlay').fadeOut(500);
+			});
+		}
+		
+		
+		 $(".video-menu li").click(function() {
+	        $(".video-menu li").removeClass("show");
+	        $(this).addClass("show")
+	    });	
+	    
+	    $(".image-vid-btn").click(function() {
+	       $('.play_audio.active, .list-group-item.active').removeClass('active');
+		   $("#audio")[0].pause();
+		   $('.audio_footer').removeClass('show'); 
+	    });	
+		
+	
+		$( ".tube-link" ).click(function() {
+			$('body').addClass('stop-scroll');
+			$('.vidFrame').attr("src", $(this).attr("vidUrl"));
+			$('.myVideo').attr("src", "empty");
+			$('.myVideo').hide();
+			$('.vidFrame').show();
+			
+			$('.play_audio.active, .list-group-item.active').removeClass('active');
+			$('.image-audio-btn.active').removeClass('active');
+			$("#audio")[0].pause();
+			$('.audio_footer').removeClass('show');
 		});
-	});
-
-	document.querySelector(".image-vid-btn").addEventListener('click', function() {
-		document.querySelector('.play_audio.active').classList.remove('active');
-		document.querySelector('.list-group-item.active').classList.remove('active');
-		document.querySelector("#audio").pause();
-		document.querySelector('.audio_footer').classList.remove('show');
-	});
 		
-	document.querySelector(".tube-link").addEventListener('click', function() {
-		document.body.classList.add('stop-scroll');
-		document.querySelector('.vidFrame').src = this.getAttribute("vidUrl");
-		document.querySelector('.myVideo').src = "empty";
-		document.querySelector('.myVideo').style.display = 'none';
-		document.querySelector('.vidFrame').style.display = 'block';
-		document.querySelector('.play_audio.active').classList.remove('active');
-		document.querySelector('.image-audio-btn.active').classList.remove('active');
-		document.querySelector("#audio").pause();
-		document.querySelector('.audio_footer').classList.remove('show');
+		
+		$( ".mp4-link" ).click(function() {
+			$('body').addClass('stop-scroll');
+			$('.vidFrame, .vidFrame').hide();
+			$('video').removeClass('hide');
+			$('.vidFrame, .vidFrame').attr("src", "empty");
+			$('.myVideo').attr("src", $(this).attr("vidURL"));
+			$('.myVideo').show(function(){
+				 document.getElementById('myVideo').play();
+			});	
+			
+			$('.play_audio.active, .list-group-item.active').removeClass('active');
+			$('.image-audio-btn.active').removeClass('active');
+			$("#audio")[0].pause();
+			$('.audio_footer').removeClass('show');
+		});
+		
+		
+		function videoChange(){
+			$('.default-video-cover').fadeOut(500);
+			
+			$('.vid-box').fadeOut(500, function(){
+				$('.vid-box').delay(500).fadeIn(500);
+			});
+		
+			$('.main-menu').delay(500).fadeOut(500, function(){
+				$('.menu-overlay').fadeOut(500);
+			});
+		}
+		
+		
+		function hideAudio(){
+			$('.audio-btn').removeClass("active");
+	        $('.audio_footer.show, .mini-audio-btn.show').removeClass("show");
+	        document.getElementById('audio').pause();
+		}
+	
+	    
+		$(".sixteen-nine-btn").click(function() {
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".sixteen-nine").addClass("show"); 
+	    });
+	    
+	    $(".widescreen-btn").click(function() {
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".widescreen-img").addClass("show"); 
+	    });
+	    
+	    $(".standard-btn").click(function() { 
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".standard-img").addClass("show");
+	    });
+	    
+	    $(".pal-btn").click(function() { 
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".pal-img").addClass("show");
+	    });
+	    
+	    $(".na-btn").click(function() {
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".nat-arch").addClass("show");
+	    });
+	    
+	    $(".vintage-wide-btn").click(function() {
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".vintage-wide").addClass("show");
+	    });
+		
+		$(".pano-btn").click(function() {
+	        $(".modal-vid").fadeIn(500);
+	        $(".vid-holder img").removeClass("show");
+	        $(".panovision").addClass("show");
+	    });
+	
+	    
+	    $(".close").click(function() { 
+	        $(".modal-vid").fadeOut(500);
+	        /*document.getElementById('myVideo').pause();*/
+	        $('.youTube, .myVideo').attr('src', '');
+	        $('body').removeClass('stop-scroll');
+	    });
+	    
+	    $( ".video-link" ).click(function() {
+			$(".audio-btn.active").removeClass("active");
+			$("#audio")[0].pause();
+			$('.audio_footer, .mini-audio-btn').removeClass('show');
+			$('body').addClass('stop-scroll');
+		});		
+	    
 	});
-		
-	document.querySelector(".mp4-link").addEventListener('click', function() {
-		document.body.classList.add('stop-scroll');
-		document.querySelector('.vidFrame').style.display = 'none';
-		document.querySelector('video').classList.remove('hide');
-		document.querySelector('.vidFrame').src = "empty";
-		document.querySelector('.myVideo').src = this.getAttribute("vidURL");
-		document.querySelector('.myVideo').style.display = 'block';
-		document.querySelector('#myVideo').play();
-		document.querySelector('.play_audio.active').classList.remove('active');
-		document.querySelector('.image-audio-btn.active').classList.remove('active');
-		document.querySelector("#audio").pause();
-		document.querySelector('.audio_footer').classList.remove('show');
-	});
-		
-	function videoChange(){
-		document.querySelector('.default-video-cover').style.display = 'none';
-		document.querySelector('.vid-box').style.display = 'none';
+	
+	
+	 window.document.onkeydown = function (e)
+	 	{
+		 	if (!e) e = event;
+		 	if (e.keyCode == 27)
+		 	{
+		 		$(".modal-vid").fadeOut(500, function(){});
+		 		$('.youTube, .myVideo').attr('src', '');
+		 		$('body').removeClass('stop-scroll');
+		 	}
+	 	};
 
-		setTimeout(function(){
-			document.querySelector('.vid-box').style.display = 'block';
-		}, 500);
-
-		setTimeout(function(){
-			document.querySelector('.main-menu').style.display = 'none';
-			document.querySelector('.menu-overlay').style.display = 'none';
-		}, 500);
-	}
-		
-	function hideAudio(){
-		document.querySelector('.audio-btn').classList.remove("active");
-		document.querySelector('.audio_footer.show').classList.remove("show");
-		document.querySelector('.mini-audio-btn.show').classList.remove("show");
-		document.querySelector('#audio').pause();
-	}
-
-	// More click handlers here...
-
-});
-
-document.addEventListener("keydown", function(event) {
-	if (event.keyCode === 27) {
-		document.querySelector(".modal-vid").style.display = 'none';
-		document.querySelector('.youTube').src = '';
-		document.querySelector('.myVideo').src = '';
-		document.body.classList.remove('stop-scroll');
-	}
-});
+})(jQuery)
